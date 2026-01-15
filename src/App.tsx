57a9eab42292c2db9ecdaf6c3e5335c5f122f955
shomik-das <dashomik@gmail.com>
2026-01-15 14:37:08 +0600

    create signup page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+import SignupPage from "./pages/auth/signup-page"
+function App() {
+  return (
+    <>
+      <SignupPage />
+    </>
+  )
+}
+
+export default App
